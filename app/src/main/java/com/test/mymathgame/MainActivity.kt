78526f9c5aa7d011f65fb5e8e0427ca6d6e5b895
umar1289 <iuzbek123@gmail.com>
2022-06-19 14:29:30 +0500

    Lottie added

--- app/src/main/java/com/test/mymathgame/MainActivity.kt
+++ app/src/main/java/com/test/mymathgame/MainActivity.kt
@@ -6,11 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.navigation.Navigation
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
-import com.test.mymathgame.databinding.FragmentSettingsBinding
+import com.google.android.gms.ads.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_settings.*
 
@@ -21,21 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val adView = AdView(this)
-
-        adView.adSize = AdSize.BANNER
-
-        adView.adUnitId = "ca-app-pub-2160316846631976/2329056349"
-
-        MobileAds.initialize(this)
-
-        mAdView = findViewById(R.id.adView)
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
-
     }
-
     override fun onSupportNavigateUp(): Boolean {
         return Navigation.findNavController(this, R.id.my_navigation_host).navigateUp()
     }
